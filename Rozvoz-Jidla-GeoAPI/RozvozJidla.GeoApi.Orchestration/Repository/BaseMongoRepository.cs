391a37e2ccb993dc695428ed8290f442878128d5
Matous Bocek <matous.bocek@massive.co>
2020-03-19 21:18:40 +0100

    fixed mongodb connection string

--- Rozvoz-Jidla-GeoAPI/RozvozJidla.GeoApi.Orchestration/Repository/BaseMongoRepository.cs
+++ Rozvoz-Jidla-GeoAPI/RozvozJidla.GeoApi.Orchestration/Repository/BaseMongoRepository.cs
@@ -14,7 +14,6 @@ namespace RozvozJidla.GeoApi.Orchestration.Repository
         public BaseMongoRepository(IConfigurationResolver<DatabaseConfiguration> configurationResolver)
         {
             var databaseConfiguration = configurationResolver.GetConfig();
-            //string connectionString = @"mongodb://rozvoz-jidla-db:7ANtrWFsZ5DnXHULL73iecMz8tjSO4TtvvgJ8IhceI0nWYH7wh2yKhJTuHQiScbefVRfmpZTAhBLCG42w7qYaw==@rozvoz-jidla-db.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@rozvoz-jidla-db@&retrywrites=false";
             MongoClientSettings settings = MongoClientSettings.FromUrl(
                 new MongoUrl(databaseConfiguration.ConnectionString)
             );
